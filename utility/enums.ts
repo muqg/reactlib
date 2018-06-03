b73b18da5bcb9f178fd33a82977f97466a76875a
Lubo <muqglu@gmail.com>
2018-06-03 17:01:53 +0300

    Added enumerations

---
+++ utility/enums.ts
@@ -0,0 +1,19 @@
+/**
+ * Widely used CSS classes for state modification.
+ */
+export enum StyleClass {
+    Active = "active",
+    Disabled = "disabled",
+}
+
+/**
+ * HTTP request method verbs.
+ */
+export enum RequestMethod {
+    GET = "GET",
+    HEAD = "HEAD",
+    POST = "POST",
+    PUT = "PUT",
+    DELETE = "DELETE",
+    PATCH = "PATCH",
+}
