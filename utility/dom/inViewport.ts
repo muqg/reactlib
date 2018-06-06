9c60e09798cf10f19d77ed4716579ec614c4149d
Lubo <muqglu@gmail.com>
2018-06-06 18:03:16 +0300

    Implemented inViewport for dom

--- utility/dom/inViewport.ts
+++ utility/dom/inViewport.ts
@@ -1,7 +1,31 @@
-// TODO: React | implement.
+/**
+ * Checks whether an element is within user's viewport. Does not work on hidden
+ * elements.
+ * @param element The element to be checked for.
+ */
+function inViewport(element: Element): boolean
+/**
+ * Checks whether an element is within user's viewport. Does not work on hidden
+ * elements.
+ * @param element The element to be checked for.
+ * @param offset Viewport offset around the element.
+ */
+function inViewport(element: Element, offset: number): boolean
 
-function inViewport() {
-    console.error("Function is not yet implemented (NYI).")
+function inViewport(element, offset = 0) {
+    const rect = element.getBoundingClientRect()
+    const viewport = {
+        top: rect.top - window.innerHeight - offset,
+        bottom: rect.bottom + offset,
+        left: rect.left - window.innerWidth - offset,
+        right: rect.right + offset
+    }
+
+    return (
+        (viewport.top <= 0 && viewport.bottom >= 0)
+        &&
+        (viewport.left <= 0 && viewport.right >= 0)
+    )
 }
 
 
