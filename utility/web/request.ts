f6ca11c6acbd78e17791f64c27c0b23be4928f33
Lubo <muqglu@gmail.com>
2018-06-09 10:38:35 +0300

    Initial implementation for request function

--- utility/web/request.ts
+++ utility/web/request.ts
@@ -1,8 +1,42 @@
-// TODO: React | implement.
+import { RequestMethod } from "../enums";
+import { createQuery } from ".";
 
-function request() {
-    console.error("Function is not yet implemented (NYI).")
+export interface RequestOptions {
+    headers?: Headers
 }
+
+interface Headers {
+    [key: string]: string
+}
+
+function request(method: RequestMethod, url: string, bodyData: object, options: RequestOptions) {
+        const headers = options.headers || {} as Headers
+
+        const requestInit: RequestInit = {
+            cache: "no-cache",
+            credentials: "same-origin",
+            headers: headers,
+            method: method,
+            mode: "cors",
+            redirect: "follow",
+            referrer: "no-referrer"
+        }
+
+        // GET and HEAD methods are not allowed to have a request body.
+        if(method === RequestMethod.GET || method === RequestMethod.HEAD) {
+            const index = url.indexOf("?")
+            const cleanURL = index >= 0 ? url.substring(0, index) : url
+            url = cleanURL + "?" + createQuery(bodyData)
+        }
+        else {
+            requestInit.body = JSON.stringify(bodyData)
+        }
+
+        return window.fetch(url, requestInit)
+            .then(function(response) {
+                return response.text()
+            })
+    }
 
 
 export default request
