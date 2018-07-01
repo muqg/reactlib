1e0f5ef83555315158da0969b798fd004fb3891d
Lubo <muqglu@gmail.com>
2018-07-01 22:35:50 +0300

    Model is now even lighter and better

--- hoc/model.tsx
+++ hoc/model.tsx
@@ -1,6 +1,5 @@
 import * as React from "react";
 import { StringDict } from "../utility";
-import { def } from "../utility/assertions";
 import { dive } from "../utility/collection";
 import { findParentWithClass, parseFormElement } from "../utility/dom";
 
@@ -18,44 +17,34 @@ interface Model<MD extends object = StringDict<any>> {
      * Handles data change of an input element and accepts a callback that may
      * handle any specific change behaviour.
      * - The callback may be async and will be awaited for.
-     * - Causes a re-render (with the new model data).
+     * - Causes a re-render.
      */
     change(event: React.ChangeEvent<HTMLElement>, callback?: ChangeCallback): Promise<void>
 
-
     /**
-     * Handles basic data submission and accepts a callback that may handle
-     * any specific behaviour.
-     * - The callback may be async and will be awaited for.
-     */
-    submit<MP extends object = MD>(event: React.SyntheticEvent<any>, callback: SubmissionCallback<MP>): Promise<void>
-
-    /**
-     * Sets the base model data to the passed object, overwriting any previous data.
-     * - Calls the reset method of the model and thus causes a re-render.
+     * Sets the base model data to the passed object, overwriting any previous
+     * data. Calls the reset method of the model and thus causes a re-render.
      */
     setBaseData(base: MD): void
 
     /**
-     * Sets a model value.
-     * - Causes a re-render (with the new model data).
+     * Sets a model value and causes a re-render.
      */
     setValue(name: string, value: any): void
 
     /**
-     * Resets to the base data and causes a re-render.
+     * Resets to the base data and causes a re-render. Any value using Model's
+     * data will be reset to its base value if one has been provided.
      */
     reset(): void
 
     /**
-     * Holds the model's data. Should be used to initially set values of elements.
-     * Callbacks should be used for any direct manipulation.
+     * Readable Model data. Use methods to set changes to Model's data.
      */
     readonly data: MD
 }
 
 type CallbackReturnValue = boolean | void | Promise<boolean> | Promise<void>
-type SubmissionCallback<SP extends object = Model["data"]> = (modelData: SP) => CallbackReturnValue
 type ChangeCallback = (newData: string, targetElement: HTMLElement) => CallbackReturnValue
 
 
@@ -63,18 +52,18 @@ type ChangeCallback = (newData: string, targetElement: HTMLElement) => CallbackR
 /**
  * Enhances a component with model serialization. See ModelProps for more info.
  * @param WrappedComponent The component to be wrapped.
- * @param baseData Model's base data.
+ * @param baseModelData Model's base data.
  */
 function CreateModel<OP extends {}, MD extends object = Model["data"]>(
     WrappedComponent: React.ComponentType<OP & ModelProps<MD>>,
-    baseData?: Model["data"]): React.ComponentType<OP>
+    baseModelData: Model["data"] = {}): React.ComponentType<OP>
 {
 
     class withModel extends React.Component<OP, Model["data"]> {
         static displayName: string
 
-        baseData = def(baseData, {}) as Model["data"]
-        state: Model["data"] = {...baseData}
+        baseData = baseModelData
+        state: Model["data"] = {...baseModelData}
 
         async handleChange(event: React.ChangeEvent<HTMLElement>, callback: ChangeCallback = () => {}) {
             const targetElement = event.target as HTMLFormElement
@@ -96,13 +85,6 @@ function CreateModel<OP extends {}, MD extends object = Model["data"]>(
             await callback(elementData, targetElement)
 
             this.setValue(name, elementData)
-        }
-
-        async handleSubmit(event: React.SyntheticEvent<any>, callback: SubmissionCallback = () => {}) {
-            event.preventDefault();
-
-            // Allow async callback.
-            await callback({...this.state})
         }
 
         setBaseData(baseData: Model["data"]) {
@@ -127,7 +109,6 @@ function CreateModel<OP extends {}, MD extends object = Model["data"]>(
                     model={{
                         data: {...this.state},
                         change: this.handleChange.bind(this),
-                        submit: this.handleSubmit.bind(this),
                         setValue: this.setValue.bind(this),
                         setBaseData: this.setBaseData.bind(this),
                         reset: this.reset.bind(this)
@@ -142,6 +123,7 @@ function CreateModel<OP extends {}, MD extends object = Model["data"]>(
     withModel.displayName = `withModel(${name})`
     return withModel
 }
+
 
 export { CreateModel as model };
 
