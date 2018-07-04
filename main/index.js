afde6aa7245f1e1b6f21ce37417d44558b4b29b0
Lubo <muqglu@gmail.com>
2018-07-04 12:23:46 +0300

    Added localizeLevel function

--- main/index.js
+++ main/index.js
@@ -1,7 +1,9 @@
 export { config } from "./config";
 export { CURRENT_LOCALE } from "./const";
 export { getURI } from "./getURI";
+export { initialState } from "./initialState";
 export { localize } from "./localize";
+export { localizeLevel } from "./localizeLevel";
 export { ReduxFactory } from "./ReduxFactory";
 export { render } from "./render";
-export { initialState } from "./initialState";
+
