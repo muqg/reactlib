1bb022b4bf0fd6f795d0dff0050a611228a93a0d
Lubo <muqglu@gmail.com>
2018-08-03 00:40:16 +0300

    Added COLOR_DARK_GREEN and COLOR_DARK_RED

--- styles/colours.ts
+++ styles/colours.ts
@@ -4,6 +4,8 @@ export const COLOR_SECONDARY_LIGHT = "#efefef"
 
 export const COLOR_BLACK = "#000"
 export const COLOR_GREEN = "#2d9e21"
+export const COLOR_DARK_GREEN = "#24821a"
 export const COLOR_RED = "#ff0033"
+export const COLOR_DARK_RED = "#cc0000"
 export const COLOR_WHITE = "#fff"
 export const COLOR_TRANSPARENT = "transparent"
