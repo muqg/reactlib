5708ed70b60a755c88b2a84ca98453a245bf621d
Lubo <muqglu@gmail.com>
2018-10-20 18:49:30 +0300

    Add SC orientation

---
+++ styles/orientation.ts
@@ -0,0 +1,28 @@
+import { css } from ".";
+import { Dict } from "../utility";
+
+interface Orientation extends Dict<any> {
+    landscape: typeof css
+    portrait: typeof css
+}
+
+const orientations = [
+    'portrait',
+    'landscape'
+]
+
+const orientation = {} as Orientation
+
+for(let o of orientations) {
+    orientation[o] = (...args: any[]) => {
+        const [first, ...rest] = args
+        return css`
+            @media (orientation: o) {
+                ${css(first, ...rest)}
+            }
+        `
+    }
+}
+
+export { orientation };
+
