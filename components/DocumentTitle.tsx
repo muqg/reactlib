2e5cce02549c75730a1ad15153f8ca676ed32d96
Lubo <muqglu@gmail.com>
2018-10-20 15:19:12 +0300

    DocumentTitle is now translateable

--- components/DocumentTitle.tsx
+++ components/DocumentTitle.tsx
@@ -1,15 +1,29 @@
+import { Translate } from "./Translation";
+import * as React from "react";
 
 interface Props {
-    children?: JSX.Element
     title: string
+    translate?: boolean
 }
 
 
-const DocumentTitle: React.StatelessComponent<Props> = ({children, title}: Props) => {
-    if(title)
-        document.title = title
+const DocumentTitle: React.StatelessComponent<Props> = ({title, translate}: Props) => {
+    if(title) {
+        if(translate) {
+            return (
+                <Translate value={title}>
+                    {text => {
+                        document.title = text
+                        return null
+                    }}
+                </Translate>
+            )
+        }
+        else
+            document.title = title
+    }
 
-    return children || null
+    return null
 }
 
 
