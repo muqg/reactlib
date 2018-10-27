45c706c31eb1dfd4ed6f07e21fac63be40036499
Lubo <muqglu@gmail.com>
2018-10-27 18:24:10 +0300

    Add getLocalStorageSize

---
+++ utility/web/getLocalStorageSize.ts
@@ -0,0 +1,17 @@
+/**
+ * Returns the total localStorage size in KB.
+ */
+function getLocalStorageSize() {
+    const content = Object.entries(localStorage).reduce(
+        (total, [key, value]) => {
+            total += key + value
+            return total
+        },
+        ""
+    )
+
+    return content ? 3 + ((content.length * 16) / (8 * 1024)) : 0;
+}
+
+export { getLocalStorageSize };
+
--- utility/web/index.js
+++ utility/web/index.js
@@ -1,4 +1,6 @@
 export { createQuery } from "./createQuery";
+export { getLocalStorageSize } from "./getLocalStorageSize";
 export { parseQuery } from "./parseQuery";
 export { request, RequestOptions } from "./request";
 export { upload } from "./upload";
+
