02b1a7d152d5a94685171f376f5fb93e8a4c4765
Lubo <muqglu@gmail.com>
2018-11-04 09:47:47 +0200

    Revert block to using BlockContent sub-component

--- components/Block.tsx
+++ components/Block.tsx
@@ -1,4 +1,4 @@
-import * as React from "react"
+import * as React from "react";
 import styled from "styled-components";
 import { Translate } from ".";
 
@@ -16,32 +16,42 @@ const Title = styled.h4`
     padding: 4px 0;
 `
 
-interface Props {
-    as?: string
-    children?: any
-    className?: string
-    title?: string
+
+interface Props extends ContainerProps {
     /**
      * Whether the title is a key to a translation text.
      */
     translate?: boolean
 }
+interface ContainerProps {
+    as?: string
+    children?: any
+    className?: string
+    title?: string
+}
 
 
-const Block = ({as, children, className, title, translate}: Props) => {
+const Block = ({translate, ...props}: Props) => {
+    if(translate && props.title) {
+        return (
+            <Translate value={props.title}>
+                {text => <BlockContent {...props} title={text} />}
+            </Translate>
+        )
+    }
+    return <BlockContent {...props} />
+}
+
+const BlockContent = ({as, className, title, children}: ContainerProps) => {
     return (
-        <Translate value={title || ""}>
-            {text => (
-                <Container className={className} as={as}>
-                    <Title>
-                        {translate ? text : title}
-                    </Title>
-                    {children}
-                </Container>
-            )}
-        </Translate>
+        <Container className={className} as={as}>
+            <Title>
+                {title}
+            </Title>
+            {children}
+        </Container>
     )
 }
 
 
-export { Block }
+export { Block };
