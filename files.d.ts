c1f0042b3096c4787e6be55c995421cc9cd5e9f0
Lubo <muqglu@gmail.com>
2018-11-07 14:58:37 +0200

    Files declaration moved to lib

---
+++ files.d.ts
@@ -0,0 +1,22 @@
+/* ----------------------------- *
+ * Declarations for file-loader. *
+ * ----------------------------- */
+declare module "*.png" {
+    const content: any;
+    export default content;
+}
+
+declare module "*.svg" {
+    const content: any;
+    export default content;
+}
+
+declare module "*.jpeg" {
+    const content: any;
+    export default content;
+}
+
+declare module "*.jpg" {
+    const content: any;
+    export default content;
+}
