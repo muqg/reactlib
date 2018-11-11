06e4f915049144d1e5662a7a72c6c5395d1f2bec
Lubo <muqglu@gmail.com>
2018-11-11 10:18:47 +0200

    Add useResource hook

--- hooks/index.ts
+++ hooks/index.ts
@@ -2,4 +2,5 @@
 export { useDocumentTitle } from "./useDocumentTitle";
 export { useGlobalHotkey } from "./useGlobalHotkey";
 export { useModel } from "./useModel";
+export { useResource } from "./useResource";
 
---
+++ hooks/useResource.ts
@@ -0,0 +1,154 @@
+import { useContext, useEffect, useState } from "react";
+import { useModel } from ".";
+import { NotificationContext } from "../components";
+import { RequestException, RequestMethod } from "../utility";
+import { isString, isType } from "../utility/assertions";
+import { request } from "../utility/web";
+
+type ResourceCallback<T extends object> = (resource: T) => string | void | Promise<string | void>
+
+export interface ResourceProps<T extends object> {
+    /**
+     * Default data for the resource. Used when a new one is being created
+     * and also when a resource is deleted and the data should be reset.
+     */
+    default: T
+    /**
+     * Text to be shown when an exceptional error occurs.
+     */
+    exceptionText?: string
+    /**
+     * Id of the resource. Falsey value for a new resource.
+     */
+    id: string | number | null | undefined
+    /**
+     * Called before deleting a resource. May return a string with an error
+     * message to show it as notification and cancel the process.
+     */
+    deleting?: ResourceCallback<T>
+    /**
+     * Called after deleting a resource. May return a string to show as
+     * notification for when deletion is done.
+     */
+    deleted?: ResourceCallback<T>
+    /**
+     * Called before saving a resource. May return a string with an error message
+     * to show it as notification and cancel the process. This makes it a perfect
+     * place for data validation.
+     */
+    saving?: ResourceCallback<T>
+    /**
+     * Called after saving a resource. May return a string to show as
+     * notification for when saving is done.
+     */
+    saved?: ResourceCallback<T>
+    /**
+     * URL to the resource (without id appended).
+     */
+    url: string
+}
+
+function useResource<T extends object>({url = document.location!.href, ...props}: ResourceProps<T>) {
+    const [isWorking, setIsWorking] = useState(false)
+    const model = useModel(props.default)
+
+    const notify = useContext(NotificationContext)
+
+    useEffect(() => {
+        if(props.id) {
+            _work(async () => {
+                const requestURL = url + "/" + props.id
+                return await request<T>(RequestMethod.GET, requestURL)
+            })
+        }
+        else {
+            // Resetting resource outside of the worker
+            // wrapper skips an unnecessary render.
+            model.$set(props.default)
+        }
+    }, [props.id])
+
+    async function save() {
+        await _work(async () => {
+            const {id, saved, saving} = props
+
+            if(saving && _error(await saving(model.$data)))
+                return
+
+            const method = id ? RequestMethod.PUT : RequestMethod.POST
+            const requestURL = url + (method === RequestMethod.PUT ? "/" + id : "")
+            const payload = JSON.stringify(model.$data)
+
+            // @ts-ignore Spread types may be created only from object types.
+            const response = await request<Partial<T>>(method, requestURL, {payload})
+
+            // @ts-ignore Spread types may be created only from object types.
+            const nextResource = {...model.$data, ...response}
+            if(saved)
+                _error(await saved(nextResource))
+            return nextResource
+        })
+    }
+
+    async function del() {
+        if(!props.id)
+            return
+
+        await _work(async () => {
+            const {deleting, deleted} = props
+            if(deleting && _error(await deleting(model.$data)))
+                return
+
+            const requestURL = url + "/" + props.id
+            const payload = JSON.stringify(model.$data)
+            await request(RequestMethod.DELETE, requestURL, {payload})
+
+            const nextResource = props.default
+            if(deleted)
+                _error(await deleted(nextResource))
+            return nextResource
+        })
+    }
+
+    function _error(err: string | void) {
+        if(isString(err)) {
+            notify(err)
+            return true
+        }
+        return false
+    }
+
+    async function _work(worker: () => Promise<T | void>) {
+        if(isWorking)
+            return
+
+        setIsWorking(true)
+        try {
+            const resource = await worker()
+            if(resource)
+                model.$set(resource)
+        }
+        catch(ex) {
+            console.error(ex)
+
+            let msg = props.exceptionText || "Error"
+            if(isType<RequestException>(ex, () => ex.status))
+                msg = ex.text
+            notify(msg)
+        }
+        setIsWorking(false)
+    }
+
+    return {
+        isWorking,
+        model,
+        save,
+
+        data: model.$data,
+        delete: del,
+    }
+}
+
+
+export { useResource };
+
