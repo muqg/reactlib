cfbe3b7756272bcc09b6902f2c06d2ff4af1aa06
Lubo <muqglu@gmail.com>
2018-11-14 16:34:07 +0200

    useModel now caches the model if it hasn't changed

--- hooks/useModel.ts
+++ hooks/useModel.ts
@@ -1,6 +1,6 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
+import { isNull, isObject, isType } from "../utility/assertions";
 import { ParseableInput, parseInputValue } from "../utility/dom";
-import { isObject, isType } from "../utility/assertions";
 import { cast } from "../utility/string";
 
 export type Model<T extends object, K extends keyof T = keyof T> = {
@@ -10,14 +10,17 @@ export type Model<T extends object, K extends keyof T = keyof T> = {
     }
 }
 export type ModelInput = ParseableInput | string | boolean | number | object
-
 export type ModelSetFunction<T extends object> = React.Dispatch<React.SetStateAction<T>>
+type ModelReturnValue<T extends object> = [Readonly<Model<T>>, T, ModelSetFunction<T>]
 
-function useModel<T extends object>(init: T): [Readonly<Model<T>>, T, ModelSetFunction<T>] {
+function useModel<T extends object>(init: T): ModelReturnValue<T> {
     const [data, setModel] = useState(init)
+    const cache = useRef<null | ModelReturnValue<T>>(null)
 
     // @ts-ignore Spread types may be created only from object types.
     function change(name: string, input: ModelInput) {
+        cache.current = null
+
         // @ts-ignore Spread types may be created only from object types.
         let value = input
         if(isType<ParseableInput>(input, (v) => v.target || isObject(v, Element)))
@@ -30,17 +33,21 @@ function useModel<T extends object>(init: T): [Readonly<Model<T>>, T, ModelSetFu
         })
     }
 
-    let model = {} as Model<T>
-    for(let key in init) {
-        // @ts-ignore Spread types may be created only from object types.
-        model[key] = {
-            value: data[key],
+    if(isNull(cache.current)) {
+        let model = {} as Model<T>
+        for(let key in init) {
             // @ts-ignore Spread types may be created only from object types.
-            onChange: c => change(key, c)
+            model[key] = {
+                value: data[key],
+                // @ts-ignore Spread types may be created only from object types.
+                onChange: c => change(key, c)
+            }
         }
+
+        cache.current = [model, data, setModel]
     }
 
-    return [model, data, setModel]
+    return cache.current
 }
 
 
