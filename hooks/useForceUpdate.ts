118d0bf0155510bbe2b744d75b70d69971603876
Lubo <muqglu@gmail.com>
2018-11-17 09:21:55 +0200

    Add useForceUpdate hook

---
+++ hooks/useForceUpdate.ts
@@ -0,0 +1,12 @@
+﻿import { useState } from "react";
+
+/**
+ * Returns a function which may be used to force a
+ * component update at any given time.
+ */
+function useForceUpdate() {
+    return useState(0)[1] as (() => void)
+}
+
+export { useForceUpdate };
+
