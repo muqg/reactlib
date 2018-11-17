476911a67f60f815ba931c07f45ad0b087d23bfb
Lubo <muqglu@gmail.com>
2018-11-17 09:19:51 +0200

    Add usePrevValue and useSwitch hooks

--- hooks/index.ts
+++ hooks/index.ts
@@ -4,6 +4,8 @@ export { useGlobalHotkey } from "./useGlobalHotkey";
 export { useInitialRender } from "./useInitialRender";
 export { useList } from "./useList";
 export * from "./useModel";
+export { usePrevValue } from "./usePrevValue";
 export { useResource } from "./useResource";
+export { useSwitch } from "./useSwitch";
 export { useTranslation } from "./useTranslation";
 
