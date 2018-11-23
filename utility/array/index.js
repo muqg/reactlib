7d20601aebeb991c2f38ae0ad0f8c87b5011e9b0
Lubo <muqglu@gmail.com>
2018-11-23 16:20:26 +0200

    Remove removeAll, useStatic and useList

--- utility/array/index.js
+++ utility/array/index.js
@@ -1,7 +1,5 @@
-export { removeAll } from "./removeAll";
 export { replace } from "./replace";
 export { replaceAll } from "./replaceAll";
 export { replaceOrPush } from "./replaceOrPush";
 export { shuffle } from "./shuffle";
-
 
