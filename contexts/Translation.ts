412381b551a7e3b32143f2d6199ee3221252e8d3
Lubo <muqglu@gmail.com>
2018-11-30 14:54:38 +0200

    TranslationContext's default value is now set to initial_state's locale value

--- contexts/Translation.ts
+++ contexts/Translation.ts
@@ -1,6 +1,7 @@
 import { createContext } from "react";
+import { initialState } from "../main";
 import { Dict } from "../utility";
 
 
-export const TranslationContext = createContext({} as Dict<any>)
+export const TranslationContext = createContext(initialState("locale") || {} as Dict<any>)
 
