f9f140e0c3a7c95d089bc28afec41efc3d6095ab
Lubo <muqglu@gmail.com>
2018-12-02 15:51:07 +0200

    Improve block styling and more translate prop

--- components/Block.tsx
+++ components/Block.tsx
@@ -1,44 +1,42 @@
 import * as React from "react";
-import { useTranslation } from "../hooks";
-import { styled } from "../styles";
+import { styled, css } from "../styles";
+import { Omit } from "../utility";
 
 
 const Container = styled.div`
-    display: block;
-    margin: 12px 0;
+    ${(_p: StyleProps) => ""}
 
-    > textarea {
-        min-height: 4em;
-        width: 100%;
-    }
+    margin: 12px 0;
+    min-width: 285px;
+    width: 100%;
+
+    ${p => p.center && css`margin: 12px auto;`}
+    ${p => p.maxWidth && css`max-width: ${p.maxWidth}px;`}
 `
 const Title = styled.h4`
     padding: 4px 0;
 `
 
-
-interface Props extends ContainerProps {
+interface StyleProps {
     /**
-     * Whether the title is a key to a translation text.
+     * Attempts to center the block.
+     * Use in combination with maxWidth prop.
      */
-    translate?: boolean
-}
-interface ContainerProps {
-    as?: keyof JSX.IntrinsicElements | React.ComponentType<any>
-    children?: any
-    className?: string
-    title?: string
+    center?: boolean
+    /**
+     * Maximum width of the block.
+     */
+    maxWidth?: number
 }
 
+// Ref is not assignable to SC 4.0.3
+type Block = Omit<React.HTMLProps<HTMLDivElement>, "ref"> & StyleProps
 
-const Block = ({translate, as, className, title = "", children}: Props) => {
-    const getTranslated = useTranslation()
 
+const Block = ({children, title, ...props}: Block) => {
     return (
-        <Container className={className} as={as}>
-            <Title>
-                {translate ? getTranslated(title) : title}
-            </Title>
+        <Container {...props}>
+            {title && <Title>{title}</Title>}
             {children}
         </Container>
     )
