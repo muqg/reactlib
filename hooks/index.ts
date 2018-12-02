e2065908fef7f449108317ae39de15b6c0c51436
Lubo <muqglu@gmail.com>
2018-12-02 17:23:11 +0200

    Remove unnecessary hooks

--- hooks/index.ts
+++ hooks/index.ts
@@ -1,15 +1,11 @@
 
-export { useCounter } from "./useCounter";
 export { useDelayed } from "./useDelayed";
 export { useDocumentTitle } from "./useDocumentTitle";
-export { useForceUpdate } from "./useForceUpdate";
 export { useGlobalHotkey } from "./useGlobalHotkey";
 export { useInitialRender } from "./useInitialRender";
 export { useLocked } from "./useLocked";
 export * from "./useModel";
-export { usePrevValue } from "./usePrevValue";
 export * from "./useResource";
 export * from "./useResourceList";
-export { useSwitch } from "./useSwitch";
 export { useTranslation } from "./useTranslation";
 
