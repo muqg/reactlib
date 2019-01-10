c717036a52baa3e775748fc7bf1512eece0fdbed
Lubo <muqglu@gmail.com>
2019-01-10 12:10:24 +0200

    DialogBox is now sizeable

--- components/dialogs/DialogBox.tsx
+++ components/dialogs/DialogBox.tsx
@@ -1,63 +1,71 @@
 import * as React from "react";
 import { CloseButton } from "..";
 import { COLOR_BACKGROUND, COLOR_DARK, styled } from "../../styles";
+import { truncate } from "../../styles/mixins";
+import { Size } from "../../utility";
 import { Dialog, DialogProps } from "./Dialog";
 
+const SIZE_FACTOR = 320
 
 const Back = styled.div`
     background: black;
-    height: 100%;
+    bottom: 0;
     left: 0;
-    opacity: .5;
+    opacity: .75;
     position: absolute;
+    right: 0;
     top: 0;
-    width: 100%;
     z-index: -1;
 `
 const Container = styled.div`
+    ${(_p: ContainerStyleProps) => ""}
+
     background: ${COLOR_BACKGROUND};
-    border-radius: 3px;
-    min-height: 54px;
-    max-height: 400px;
+    max-height: 90vh;
+    max-width: ${p => p.size * SIZE_FACTOR}px;
     padding: 12px;
     position: relative;
-    width: 300px;
+    width: 100%;
 `
 const Title = styled.p`
     &:not(:empty) {
         border-bottom: 1px solid;
-        border-image: linear-gradient(to right, ${COLOR_DARK}, ${COLOR_DARK}, ${COLOR_BACKGROUND}) 1;
+        border-image: linear-gradient(to right, transparent, ${COLOR_DARK}, transparent) 1;
         font-weight: bold;
         margin-bottom: 27px;
-        overflow: hidden;
         padding-bottom: 3px;
-        text-overflow: ellipsis;
-        white-space: nowrap;
-        width: 85%;
+        ${truncate("85%")}
     }
 `
 
+interface ContainerStyleProps {
+    size: Size
+}
+
 export interface DialogBoxProps {
-    children?: Dialog["children"]
+    children: Dialog["children"]
+    size?: Size
     /**
      * Dialog's title.
      */
     title?: string
 }
 
-const DialogBox = (props: DialogBoxProps & DialogProps) => {
-    return(
+type Props = DialogBoxProps & DialogProps
+
+const DialogBox: React.ComponentType<Props> = ({size = Size.Small, className, ...props}) => {
+    return (
         <Dialog {...props}>
             {close =>
                 <>
                     <Back onClick={close} />
-                    <Container>
+                    <Container size={size} className={className}>
                         <CloseButton onClick={close} />
                         <div>
                             <Title>
                                 {props.title}
                             </Title>
-                            {props.children && props.children(close)}
+                            {props.children(close)}
                         </div>
                     </Container>
                 </>
@@ -65,9 +73,6 @@ const DialogBox = (props: DialogBoxProps & DialogProps) => {
         </Dialog>
     )
 }
-
-
-
 
 
 export { DialogBox };
