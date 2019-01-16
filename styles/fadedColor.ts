01bab843eb56a0056d1fee32be228d52e44e6315
Lubo <muqglu@gmail.com>
2019-01-16 17:35:50 +0200

    Increase fadedColor's transparency from .1 to .12

--- styles/fadedColor.ts
+++ styles/fadedColor.ts
@@ -2,7 +2,7 @@ import { rgbColor } from "../utility/dom";
 
 function fadedColor(hexColor: string) {
     const c = rgbColor(hexColor)
-    return `rgba(${c.red},${c.green},${c.blue},.1)`
+    return `rgba(${c.red},${c.green},${c.blue},.12)`
 }
 
 export { fadedColor };
