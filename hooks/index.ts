0b063e31241b2fd4fb34c6ef46b7c03e07d0a238
Lubo <muqglu@gmail.com>
2019-02-04 16:54:36 +0200

    Add useInterval hook

--- hooks/index.ts
+++ hooks/index.ts
@@ -3,6 +3,7 @@ export { useDebounced } from "./useDebounced";
 export { useDocumentTitle } from "./useDocumentTitle";
 export { useGlobalHotkey } from "./useGlobalHotkey";
 export { useInitialRender } from "./useInitialRender";
+export { useInterval } from "./useInterval";
 export { useLocked } from "./useLocked";
 export * from "./useModel";
 export * from "./useResource";
---
+++ hooks/useInterval.ts
@@ -0,0 +1,32 @@
+import { useEffect, useRef } from "react";
+
+/**
+ * Sets up an interval that executes the most recently supplied callback.
+ * The interval may be paused and its delay can be dynamically adjusted.
+ *
+ * @param callback A callback to be passed to the interval.
+ * @param delay Interval delay in milliseconds or null to stop/pause the interval.
+ */
+function useInterval(callback: () => any, delay: number | null) {
+    const savedCallback = useRef(callback)
+
+    // Remember the latest callback.
+    useEffect(() => {
+        savedCallback.current = callback
+    }, [callback])
+
+    // Set up the interval.
+    useEffect(() => {
+        function tick() {
+            savedCallback.current()
+        }
+
+        if (delay !== null) {
+            const id = setInterval(tick, delay)
+            return () => clearInterval(id)
+        }
+    }, [delay])
+}
+
+export { useInterval };
+
