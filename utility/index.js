8f6b3576fae0b918c1c2c5365380cfc10a344272
Lubo <muqglu@gmail.com>
2019-03-30 10:29:12 +0200

    Add logInfo utility

--- utility/index.js
+++ utility/index.js
@@ -2,6 +2,7 @@ export { createPointer } from "./createPointer";
 export { delay } from "./delay";
 export * from "./enums";
 export { lock } from "./lock";
+export { logInfo } from "./logInfo";
 export * from "./type";
 export { until } from "./until";
 export { wait } from "./wait";
---
+++ utility/logInfo.ts
@@ -0,0 +1,9 @@
+/**
+ * Logs information to the console, preceding it with [INFO] notice.
+ */
+function logInfo(...args: any) {
+    console.log("[INFO]", ...args)
+}
+
+export { logInfo };
+
