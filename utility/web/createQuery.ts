af8641e51cb85436139b0bd5df690cd77823782e
Lubo <muqglu@gmail.com>
2019-04-06 00:44:12 +0300

    Fix createQuery retaining undefined values

--- utility/web/createQuery.ts
+++ utility/web/createQuery.ts
@@ -3,11 +3,14 @@
  * @param params Object with parameters.
  */
 export function createQuery(params: object): string {
-    return Object.entries(params)
-        .map(
-            ([key, value]) =>
-                encodeURIComponent(key) + "=" + encodeURIComponent(value)
-        )
-        .filter(p => p)
-        .join("&")
+    return (
+        Object.entries(params)
+            .filter(([_, value]) => value !== undefined)
+            .map(
+                ([key, value]) =>
+                    encodeURIComponent(key) + "=" + encodeURIComponent(value)
+            )
+            // .filter(p => p)
+            .join("&")
+    )
 }
