f1aa310ce6c303c928ca72e32c7bccca4fa62ed6
Lubo <muqglu@gmail.com>
2019-04-05 18:31:21 +0300

    Add validate utility function

---
+++ utility/validate.ts
@@ -0,0 +1,30 @@
+import {call} from "./function"
+import {Dictionary} from "./type"
+
+export type ValidationError = string | undefined | void
+
+/**
+ * Validates an object of values.
+ * @param values Value map.
+ * @param validators A map of value validator functions.
+ */
+function validate<T extends object>(
+    values: T,
+    validators: Partial<
+        Dictionary<T, (value: any, values: T) => ValidationError>
+    >
+): Partial<Dictionary<T, ValidationError>> {
+    const errors: Partial<Dictionary<T, ValidationError>> = {}
+    for (const name in values) {
+        if (name in validators) {
+            const res = call(validators[name], values[name], values)
+            if (res) {
+                errors[name] = res
+            }
+        }
+    }
+
+    return errors
+}
+
+export {validate}
