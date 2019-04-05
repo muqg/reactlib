6077d29bd8357646d37369e5820e29f58743e3a7
Lubo <muqglu@gmail.com>
2019-04-05 17:06:32 +0300

    Add isSyntheticEvent utility

---
+++ utility/react/isSyntheticEvent.ts
@@ -0,0 +1,10 @@
+import {SyntheticEvent} from "react"
+
+function isSyntheticEvent(val: any): val is SyntheticEvent {
+    // Every truthy value has a constructor name since everything
+    // is an object and therefore the line below is unnecessary.
+    return val && val.constructor.name === "SyntheticEvent"
+    // return isObject(val) && val.constructor.name === "SyntheticEvent"
+}
+
+export {isSyntheticEvent}
