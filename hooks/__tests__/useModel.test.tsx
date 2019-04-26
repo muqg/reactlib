c43726c21f30f22df10f5a5e9c18c9500b9214be
Lubo <muqglu@gmail.com>
2019-04-26 14:08:34 +0300

    Add model binding
    Allows a model hook to accept a binder callback as as second argument. It is called whenever changes on the model occur and it is allowed to handle them and update the component tree instead of the model itself.
    
    This allows for a performance improvement when child models have to be passed on to a parent, while the parent updates this child component whenever its data is passed back up. It skips an intermediary update of the child.
    
    In order to achieve this state is now stored in a ref object instead of a reducer and updates are forcefully called whenever necessary.
    
    - Adds tests to confirm that the model updates correctly.
    - modelHookReducer's logic has been moved into the useModel's file in order to be more tightly coupled

--- hooks/__tests__/useModel.test.tsx
+++ hooks/__tests__/useModel.test.tsx
@@ -3,8 +3,14 @@ import {renderHook} from "react-hooks-testing-library"
 import {act, cleanup, fireEvent, render} from "react-testing-library"
 import {useModel} from "../useModel"
 
-const modelHook = () =>
-    useModel(() => ({
+/**
+ * Names of model methods that should force an update on owner component
+ * or pass data to binder and let it update instead.
+ */
+const modelUpdateMethods = ["onChange", "onBlur", "$change", "$reset"]
+
+const modelHook = () => {
+    return useModel(() => ({
         undef: undefined,
         parsed: {
             value: 0,
@@ -17,13 +23,33 @@ const modelHook = () =>
             },
         },
     }))
+}
+
+const MockModelComponent = ({
+    updateCounter,
+    binder,
+}: {
+    updateCounter: jest.Mock
+    binder?: (val: any) => void
+}) => {
+    const model = useModel(() => ({test: ""}), binder)
+    updateCounter()
+
+    return (
+        <>
+            <button onClick={() => model.test.onChange("asd")}>onChange</button>
+            <button onClick={model.test.onBlur}>onBlur</button>
+            <button onClick={() => model.$change({test: 2})}>$change</button>
+            <button onClick={() => model.$reset()}>$reset</button>
+        </>
+    )
+}
 
 describe("Model hook", () => {
     let model = renderHook(modelHook).result
 
     beforeEach(() => {
         cleanup()
-
         model = renderHook(modelHook).result
     })
 
@@ -70,9 +96,9 @@ describe("Model hook", () => {
 
     it("validates values on element blur", () => {
         const {getByTestId} = render(
-            <input {...model.current.validated} data-testid="test_input" />
+            <input {...model.current.validated} data-testid="input" />
         )
-        fireEvent.blur(getByTestId("test_input"))
+        fireEvent.blur(getByTestId("input"))
 
         expect(model.current.validated.error).toBe("error")
     })
@@ -90,4 +116,70 @@ describe("Model hook", () => {
 
         expect(entryBeforeValidation).not.toBe(model.current.validated)
     })
+
+    it("parses input argument of type Model as model.$data() when no custom parser is present", () => {
+        const {result: parent} = renderHook(() =>
+            useModel(() => ({
+                bound: "",
+            }))
+        )
+        const {result: child} = renderHook(() =>
+            useModel<{id: string; test: number}>(
+                () => ({
+                    id: "asd",
+                    test: 10,
+                }),
+                parent.current.bound.onChange
+            )
+        )
+        act(() => child.current.test.onChange(100))
+
+        expect(parent.current.bound.value).toMatchObject({id: "asd", test: 100})
+    })
+
+    it.each(modelUpdateMethods)(
+        "updates owner component when $method is called",
+        method => {
+            const counter = jest.fn()
+            const dom = render(<MockModelComponent updateCounter={counter} />)
+
+            act(() => {
+                fireEvent.click(dom.getByText(method))
+            })
+
+            expect(counter).toHaveBeenCalledTimes(2)
+        }
+    )
+
+    it.each(modelUpdateMethods)(
+        "skips update on owner component when binder is present and $method is called",
+        method => {
+            const childCounter = jest.fn()
+            const parentCounter = jest.fn()
+            const Parent = () => {
+                const model = useModel(() => ({
+                    childData: {
+                        value: {},
+                    },
+                }))
+
+                parentCounter()
+
+                return (
+                    <MockModelComponent
+                        updateCounter={childCounter}
+                        binder={model.childData.onChange}
+                    />
+                )
+            }
+            const dom = render(<Parent />)
+
+            act(() => {
+                fireEvent.click(dom.getByText(method))
+            })
+
+            expect(parentCounter).toHaveBeenCalledTimes(2)
+            expect(childCounter).toHaveBeenCalledTimes(2)
+        }
+    )
 })
--- hooks/useModel.ts
+++ hooks/useModel.ts
@@ -1,21 +1,32 @@
-import {useReducer} from "react"
+import {useRef} from "react"
 import {Dictionary, List, Omit, Serializable, ValidationError} from "../utility"
-import {isObject} from "../utility/assertions"
-import {except, len} from "../utility/collection"
-import {ParseableInput} from "../utility/dom"
+import {isObject, isType} from "../utility/assertions"
+import {except, isEmpty, len} from "../utility/collection"
+import {ParseableInput, parseInputValue} from "../utility/dom"
 import {call} from "../utility/function"
-import {
-    modelChangeAction,
-    modelHookReducer,
-    modelValidateAction,
-} from "../__internal__/modelHookReducer"
+import {Action, createAction, isSyntheticEvent} from "../utility/react"
+import {cast} from "../utility/string"
+import {useForceUpdate} from "./useForceUpdate"
 
-export type ModelInput = ParseableInput | Serializable
+const MODEL_OBJECT_SYMBOL_TAG =
+    typeof Symbol === "function" ? Symbol("model") : "$$model"
+
+export type ModelInput = ParseableInput | Serializable | Model<object>
 export type ModelValueList<T extends object = object> = Dictionary<
     T,
     T[keyof T]
 >
-export type ModelUtils = List<Omit<ModelElement<any>, "value">>
+
+interface ModelState {
+    // Attempts to prevent unnecessary validation.
+    hasChanged: boolean
+    model: Model<any>
+    utils: List<Omit<ModelElement<any>, "value">>
+}
+
+type ModelAction =
+    | Action<ModelValueList<any>, "change">
+    | Action<undefined, "validate">
 
 export interface ModelElement<T extends object = object> {
     /**
@@ -93,17 +104,40 @@ export type Model<T extends object> = Required<Dictionary<T, ModelEntry>> & {
     $reset(...names: Array<keyof T>): void
 }
 
-function useModel<T extends object>(
+/**
+ * A model instance maps form and other values to a predefined object structure.
+ * @param initialElements Initial model structure.
+ * @param binder A binder function which is called whenever the model should
+ * update. If it is present then the model will not update its owner component
+ * and instead pass the model instance to the binder and let it decide when and
+ * how to update.
+ *
+ * This is useful when model model instances from children have
+ * to be mirrored (chained) to a value of a parent model.
+ */
+export function useModel<T extends object>(
     initialElements: () => Partial<
         Dictionary<T, string | boolean | number | null | ModelElement<T>>
-    >
+    >,
+    binder?: (model: Model<T>) => void
 ): Model<T> {
-    const [state, dispatch] = useReducer(modelHookReducer, {}, initialize)
+    const forceUpdate = useForceUpdate()
+    const state = useRef({} as ModelState)
 
-    function initialize() {
+    if (isEmpty(state.current)) {
+        const dispatch = (action: ModelAction) => {
+            state.current = reducer(state.current, action)
+
+            if (binder) {
+                // Skip update and let the binder do all the hard work.
+                binder(state.current.model)
+            } else {
+                forceUpdate()
+            }
+        }
+
         const elements = initialElements()
-
-        const utils: ModelUtils = {}
+        const utils: ModelState["utils"] = {}
         const model = {
             $change(values: ModelValueList<any>) {
                 dispatch(modelChangeAction(values))
@@ -148,6 +182,8 @@ function useModel<T extends object>(
                 dispatch(modelValidateAction())
             },
         } as Model<any>
+        // @ts-ignore Sneak in the model object symbol tag past the typings.
+        model[MODEL_OBJECT_SYMBOL_TAG] = MODEL_OBJECT_SYMBOL_TAG
 
         for (const name in elements) {
             const currentElement = elements[name]
@@ -211,10 +247,117 @@ function useModel<T extends object>(
             } as ModelEntry
         }
 
-        return {model, utils, hasChanged: true}
+        state.current = {
+            model,
+            utils,
+            hasChanged: true,
+        }
     }
 
-    return state.model
+    return state.current.model
 }
 
-export {useModel}
+function reducer(state: ModelState, action: ModelAction): ModelState {
+    const {utils} = state
+
+    switch (action.type) {
+        case "change": {
+            const values = action.value
+            // Bail out of rendering if there are no values to update.
+            if (!len(values)) {
+                return state
+            }
+
+            const model = {...state.model}
+            for (const name in values) {
+                const entry = {...model[name]}
+                const {parse} = utils[name]
+
+                let value = values[name]
+                if (parse) {
+                    value = parse(value, entry.value)
+                } else if (
+                    isType<ParseableInput>(
+                        value,
+                        v => isSyntheticEvent(v) || v instanceof Element
+                    )
+                ) {
+                    value = cast(parseInputValue(value))
+                } else if (isModelObject(value)) {
+                    value = value.$data()
+                }
+
+                entry.value = value
+                model[name] = entry
+
+                if (__DEV__) {
+                    if (!(name in model)) {
+                        console.error(
+                            "Attempting to update a model value for name which was " +
+                                "not part of the initial structure. There is a typo in " +
+                                "your code or you forgot to include a model entry with " +
+                                `name ${name}.`
+                        )
+                    }
+                }
+            }
+
+            return {
+                ...state,
+                model,
+                hasChanged: true,
+            }
+        }
+        case "validate": {
+            if (!state.hasChanged) {
+                return state
+            }
+
+            const model = {...state.model}
+            const data = model.$data()
+
+            for (const name in utils) {
+                const entry = {...model[name]}
+                entry.error = call(
+                    utils[name].validate,
+                    model[name].value,
+                    data
+                )
+                model[name] = entry
+            }
+
+            return {
+                ...state,
+                model,
+                hasChanged: false,
+            }
+        }
+        default: {
+            if (__DEV__) {
+                throw "Dispatched invalid model action"
+            }
+            return state
+        }
+    }
+}
+
+function modelChangeAction(values: ModelValueList<any>) {
+    // This aims to prevent issues when dispatch is called asynchronously, after
+    // an input event has been cleaned up, by persisting any input synthetic events.
+    Object.values(values).forEach(v => isSyntheticEvent(v) && v.persist())
+
+    return createAction("change", values)
+}
+
+function modelValidateAction() {
+    return createAction("validate", undefined)
+}
+
+function isModelObject<T extends object = object>(val: any): val is Model<T> {
+    return (
+        isObject<any>(val) &&
+        MODEL_OBJECT_SYMBOL_TAG in val &&
+        // Works perfectly fine without typings in javascript.
+        val[MODEL_OBJECT_SYMBOL_TAG] === MODEL_OBJECT_SYMBOL_TAG
+    )
+}
