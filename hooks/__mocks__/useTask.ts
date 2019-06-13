544dc3b69f9ec72b1e71d561e09b1aed9c5d0a61
Lubo <muqglu@gmail.com>
2019-06-13 09:12:10 +0300

    Rework mocked Task hook to use act()
    This requires React 16.9-alpha+ to work as expected

--- hooks/__mocks__/useTask.ts
+++ hooks/__mocks__/useTask.ts
@@ -1,34 +1,25 @@
+import {act} from "react-testing-library"
+
 const {useTask} = jest.requireActual("../useTask")
 
-// TODO: Lib | Consider better useTask mock implementation
-// Mocking useTask should make it run synchronously with no Promises involved.
+function useMockTask(fn: () => any) {
+  const task = useTask(fn)
+  const cloneTask = {...task}
+
+  cloneTask.run = (...args: any) => {
+    let res: any
+    // TODO: Lib | Use async act with React 16.9+
+    act(() => {
+      // TODO: Lib | Await for task.run when implementing async act
+      res = task.run(...args)
+    })
+
+    return res
+  }
+
+  return cloneTask
+}
+
 module.exports = {
-  useTask: (fn: (...args: any) => any) => {
-    return {
-      ...useTask(fn),
-      run: (...args: any) => {
-        let task = fn(...args)
-
-        if (task instanceof Promise) {
-          throw "Mocked Task hook received a Promise as its callback result. " +
-            "Please mock its callback to run synchronously as well."
-        }
-
-        if (
-          typeof task[Symbol.iterator] === "function" ||
-          typeof task[Symbol.asyncIterator] === "function"
-        ) {
-          task = task as IterableIterator<any>
-          while (true) {
-            const {done, value} = task.next()
-            if (done) {
-              return done ? value : null
-            }
-          }
-        }
-
-        return task
-      },
-    }
-  },
+  useTask: useMockTask,
 }
