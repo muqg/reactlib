76ad769fe261b0a5ee39334c2629fadb07fc4c72
Lubo <muqglu@gmail.com>
2019-07-19 23:37:18 +0300

    Improved Jest setup

--- testing/setup.js
+++ testing/setup.js
@@ -1,5 +1,27 @@
+// Always run Jest in production mode.
 global.__DEV__ = false
+process.env.NODE_ENV = "production"
 
-if (document !== undefined) {
+if (typeof document !== undefined) {
   document.execCommand = () => {}
+}
+
+if (typeof window !== undefined) {
+  global.requestAnimationFrame = callback => {
+    setTimeout(callback)
+  }
+
+  global.requestIdleCallback = callback => {
+    return setTimeout(() => {
+      callback({
+        timeRemaining() {
+          return Infinity
+        },
+      })
+    })
+  }
+
+  global.cancelIdleCallback = id => {
+    clearTimeout(id)
+  }
 }
